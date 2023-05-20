7e90efda919f1030ffcdf5dfb7292d95e789c004
Vladyslav Maksymenko <maksymenko.vladyslav.o@hneu.net>
2023-05-20 10:36:10 +0300

    Add folder Models and create model "Image"

---
+++ TestTaskWebstick/Models/Image.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestTaskWebstick.Models
+{
+    public class Image
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Url { get; set; }
+    }
+}
